0787ddb4d639153d49a15d83da74eb9bee79848f
Christian <c.ship.johnson@gmail.com>
2025-02-21 05:28:30 -0500

    Updating org

--- modules/org-module.el
+++ modules/org-module.el
@@ -48,15 +48,18 @@
 
 (setq org-tag-alist
       '((:startgrouptag)
-	("F24")
+	("S24")
 	(:grouptags)
-	("DataStructures" . ?D)
-	("Comms" . ?C)
+	("EngineeringEthics" . ?E)
+	("Professional Maritime Officer" . ?M)
+	("IndustrialControls" . ?I)
+	("GlobalChallenges" . ?G)
+	("Capstone" . ?C)
         (:endgroup)
 	
-        ("@errand" . ?E)
-        ("@home" . ?H)
-        ("@work" . ?W)
+        ("@errand" . ?e)
+        ("@home" . ?h)
+        ("@work" . ?w)
         ("agenda" . ?a)
         ("planning" . ?p)
         ("publish" . ?P)
@@ -288,7 +291,7 @@
     	 "* TEST %?\n DEADLINE: %^t")
     	("sq" "Quiz" entry (file+olp "~/org/Tasks-School.org" "Study")
     	 "* QUIZ %?\n DEADLINE: %^t")
-    	("sa" "Appointment" entry (file+olp "~/org/Tasks-school" "Appointents")
+    	("sa" "Appointment" entry (file+olp "~/org/Tasks-School.org" "Appointments")
     	 "* %? \n SCHEDULED: %^t")
 	
         ("m" "Email Workflow")
